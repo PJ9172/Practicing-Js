d6d42dbd4d8bbbffde678ebc5361f527e37ed02a
Prajwal <147252354+PJ9172@users.noreply.github.com>
2023-10-16 23:38:24 +0530

    oct-16-2023

---
+++ oct_16.js
@@ -0,0 +1,9 @@
+var name=['fara','mosin','ashwini','sanjana','prajwal','shara'];
+
+//removeing first element from array
+function fremove()
+{
+    name.shift();
+    console.log(name);
+}
+fremove();
